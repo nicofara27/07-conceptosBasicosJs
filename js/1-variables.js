19fd1b89b8d95c787b8de3007e2892b12bb8e691
NicoFara <nicolas.fara.romero@gmail.com>
2022-06-29 11:14:38 -0300

    Agregamos el codigo para mostrar diferentes mensajes

---
+++ js/1-variables.js
@@ -0,0 +1,17 @@
+// Esto es un comentario de una linea
+
+/* Esto es un comentario
+de multiples
+lineas */
+
+// Mostrar un mensaje en el documento html
+document.write("<h1>Hola mundo</h1>");
+
+// Mostrar un mensaje en la consola
+console.log('Nuevo mensaje');
+
+// Mostrar un mensaje en una ventana emergente
+alert("Prueba de mensaje en ventana emergente");
+
+// Mostrar una suma 29 + 6
+docuemnt.write("El resultado de la suma es: " + (29+6));
